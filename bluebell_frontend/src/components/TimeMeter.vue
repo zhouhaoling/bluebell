d6724638b6f16f04ba48a9ff732b865cc1e1e283
zhou <1735950045@qq.com>
2024-03-20 14:00:30 +0800

    前端

---
+++ bluebell_frontend/src/components/TimeMeter.vue
@@ -0,0 +1,48 @@
+<template>
+	<div>
+		<el-card>
+			<div slot="header" class="clearfix">
+				<div class="web-status">🚀 网站状况</div>
+			</div>
+			<div class="run-time"></div>
+		</el-card>
+	</div>
+</template>
+
+<script>
+import { runTime } from "@/utils/runTime"
+export default {
+	name: 'TimeMeter',
+	data() {
+		return {
+			runTime: new Date("2023-4-14")
+		}
+	},
+	mounted() {
+		this.init();
+	},
+	methods: {
+		init() {
+			runTime({
+				el: '.run-time',
+				timeStamp: this.runTime.getTime(),
+				desc: '运行时间：'
+			});
+		}
+	}
+}
+</script>
+
+<style lang="less" scoped>
+.web-status {
+	font-weight: 600;
+	text-align: center;
+}
+
+.run-time {
+	background-image: linear-gradient(to right, orange, purple);
+	-webkit-background-clip: text;
+	color: transparent;
+	text-align: center;
+}
+</style>
